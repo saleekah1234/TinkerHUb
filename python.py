c42034d4a83008d0dc918155b7c109c94fd63e39
saleekah1234 <60786732+saleekah1234@users.noreply.github.com>
2020-03-17 17:20:30 +0530

    python class for tech learners and mentors.

---
+++ python.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import time;  # This is required to include time module.
+
+class Mentorsndlearner:
+   'Common base class'
+   mentorcount=0
+   learnercount=0
+
+
+
+
+   def __init__(stack,name,position,time,interest,expertise):
+      self.name = name
+      self.position = position
+      self.time=tim
+      self.interest=interest
+      self.expertise=expertise
+     	if(position="mentor")
+	{
+		mentorcount=mentorcount+1
+		self.time=time
+	}
+	else
+	{
+		learnercount=learnercount+1
+	}
+  def addStacks()
+  {
+	stack []
+	stack.append('sara') 
+        stack.append('lana') 
+       stack.append('roby') 
+  }
+  
+   
+  def display(self):
+       print "Name : ", self.name,  ", Position: ", self.position
+   def setMentorOrLearner() 
+	{
+		
+		if(position="mentor")
+		{
+			print "%d is mentor",self.name
+		}
+		else
+		{
+			print "%d is learner",self.name
+		}
+   def setAvailableTime():
+	{
+		int time;
+		print" time",time
+	}
+
+   def getMentor(addstack(),self.time):
+	{
+		print"Mentor avaliabale is %d",self.name,"mentorcount:",Mentorsndlearner.mentorcount);
+	}
+ #create objects
+
+   "This would create first object of  class"
+p1 = Mentorsndlearner("Zara", "learner","python")
+p2 = Mentorsndlearner("Zara", "mentor","python")
+p3=Mentorsndlearner("Zara", "learner","web")
+p4=Mentorsndlearner("Zara", "mentor","","2:00pm","python")
+
+#accessing attributes
+
+p4().setAvailableTime()
+
+
+
+
+	
+  
